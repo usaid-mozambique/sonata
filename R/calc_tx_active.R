cf34cd8d281727b79bb7375bf7f5dc1e2f81e7c4
Joseph Lara <joseph.lara@gmail.com>
2024-12-11 16:38:26 +0200

    add infrastructure

---
+++ R/calc_tx_active.R
@@ -0,0 +1,74 @@
+#' Calculate line-level listing for patients active on ART
+#'
+#' @param con MozART 2.0 database connection
+#' @param enddate Close date for the reporting period for which active on ART is calculated
+#' @param defaulter_tolerance Number of days of tolerance before a defaulting patient is considered inactive
+#' @param filter_by_location Logical indicating whether to filter query to a specific location (health facility) or not
+#' @param location_uuid Location_uuid of the health facility to filter by when filter_by_location is TRUE
+#'
+#' @return A data frame containing line-level listing of patients active on ART
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'
+#'  df <- calc_tx_active()}
+
+calc_tx_active <- function(con, enddate, defaulter_tolerance = 28, filter_by_location = FALSE, location_uuid = '4be5f1a9-832c-4717-be41-ef4b6311c0ef') {
+
+  # ARV pickup cutoff definitions and ceilings.
+  next_pickup_date_cutoff <- as.character(as.Date(enddate) - lubridate::days(defaulter_tolerance))
+  medication_pickup_date_cutoff <- as.character(as.Date(enddate) - lubridate::days(defaulter_tolerance))
+  ceiling <- as.Date(enddate) + lubridate::years(1)
+
+  # Partitioning based on calendar year
+  partition_current <- as.character(lubridate::year(enddate))
+  partition_previous <- as.character(lubridate::year(as.Date(enddate) - lubridate::years(1)))
+  partition_next <- as.character(lubridate::year(as.Date(enddate) + lubridate::years(1)))
+
+  # Conditionally include the location filter
+  location_condition <- if (filter_by_location) {
+    paste("m.location_uuid = '", location_uuid, "' AND", sep = "")
+  } else {
+    ""
+  }
+
+  sql_query <- paste(
+    "WITH med_dispensations AS (
+    SELECT
+    m.location_uuid,
+    m.encounter_uuid,
+    m.patient_uuid,
+    p.birthdate,
+    p.gender,
+    m.form_id,
+    m.regimen_id,
+    m.mode_dispensation_id,
+    m.medication_pickup_date,
+    m.next_pickup_date,
+    ROW_NUMBER() OVER (PARTITION BY p.patient_uuid ORDER BY m.next_pickup_date DESC, m.medication_pickup_date DESC) AS row_num
+    FROM
+    medication PARTITION(medication_y", partition_previous, ", medication_y", partition_current, ", medication_y", partition_next, ") m
+    LEFT JOIN patient p ON m.patient_uuid = p.patient_uuid
+    WHERE
+    ", location_condition, "
+    (medication_pickup_date <= '", enddate, " 00:00:00') AND (
+    (m.next_pickup_date >= '", next_pickup_date_cutoff, " 00:00:00' AND m.next_pickup_date <= '", ceiling, " 00:00:00' AND m.form_id = 130) OR
+    (m.medication_pickup_date >= '", medication_pickup_date_cutoff, " 00:00:00' AND m.form_id = 166)))
+
+    SELECT *
+    FROM med_dispensations
+    WHERE row_num = 1;",
+    sep = ""
+  )
+
+  # Execute the SQL query and return the result
+  df <- DBI::dbGetQuery(con, sql_query) |>
+    dplyr::mutate(enddate = enddate) |>
+    dplyr::mutate(dplyr::across(c(medication_pickup_date, next_pickup_date), ~ lubridate::as_date(ymd_hms(.)))) |>
+    dplyr::select(!row_num)
+
+
+  return(df)
+
+}
---
+++ R/calc_tx_new.R
@@ -0,0 +1,65 @@
+#' Calculate line-level listing for patients newly initiated on ART
+#'
+#' @param con MozART 2.0 database connection
+#' @param opendate Open date for the reporting period for which new ART initiations are calculated
+#' @param enddate Close date for the reporting period for which new ART initiations are calculated
+#' @param filter_by_location Logical indicating whether to filter query to a specific location (health facility) or not
+#' @param location_uuid Location_uuid of the health facility to filter by when filter_by_location is TRUE
+#'
+#' @return A data frame containing line-level listing of patients newly initiated on ART
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'
+#'  df <- calc_tx_new()}
+
+calc_tx_new <- function(con, opendate, enddate, filter_by_location = FALSE, location_uuid = '4be5f1a9-832c-4717-be41-ef4b6311c0ef') {
+
+  # conditionally include the location filter
+  location_condition <- if (filter_by_location) {
+    paste("o.location_uuid = '", location_uuid, "' AND", sep = "")
+  } else {
+    ""
+  }
+
+  # define query
+  sql_query <- paste(
+    "SELECT
+    encounter_uuid,
+    encounter_date,
+    form_id,
+    encounter_type,
+    patient_uuid,
+    encounter_created_date,
+    location_uuid,
+    concept_id,
+    observation_date,
+    value_datetime,
+    obs_uuid,
+    source_database",
+    "FROM observation o",
+    "WHERE",
+    location_condition,  # dynamic location condition
+    "concept_id = 1190 AND observation_date >= '2018-01-01'"
+  )
+
+  # execute sql query, munge, and return result
+  df <- DBI::dbGetQuery(con, sql_query) |>
+    dplyr::mutate(
+      art_start_date = dplyr::case_when(
+        value_datetime < as.Date("1980-01-01") ~ encounter_date,
+        observation_date < as.Date("1980-01-01") ~ encounter_date,
+        TRUE ~ value_datetime
+      )
+    ) |>
+    dplyr::arrange(patient_uuid, art_start_date) |>
+    dplyr::group_by(patient_uuid) |>
+    slice_head(n = 1) |>
+    dplyr::ungroup() |>
+    dplyr::filter(art_start_date > opendate & art_start_date < enddate) |>
+    dplyr::mutate(dplyr::across(c(value_datetime, art_start_date, observation_date), ~ lubridate::as_date(lubridate::ymd_hms(.))))
+
+  return(df)
+
+}
