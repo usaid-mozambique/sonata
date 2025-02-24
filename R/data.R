540c8d5bdc03c2630b4cf9949a5d83e9185dd78a
Joseph Lara <joseph.lara@gmail.com>
2025-02-24 11:44:04 +0200

    **fix** work on pkgdown site

--- R/data.R
+++ R/data.R
@@ -1,4 +1,4 @@
-#' Tabela de referência: location_uuid
+#' Referência: location_uuid
 #'
 #' Source: \href{https://github.com/usaid-mozambique/sonata/blob/main/R/data.R}{`R/data.R`}.
 #'
@@ -7,45 +7,45 @@
 #' @format Quadro de dados com variáveis:
 #' \describe{
 #'   \item{location_uuid}{código único de identificação do local}
-#'   \item{data_location_lookup}{datim_id}
-#'   \item{data_location_lookup}{sisma_id}
-#'   \item{data_location_lookup}{province}
-#'   \item{data_location_lookup}{district}
-#'   \item{data_location_lookup}{datim_name}
-#'   \item{data_location_lookup}{mozart_name}
+#'   \item{datim_id}{Código único de identificação do Datim do local}
+#'   \item{sisma_id}{Código único de identificação do SISMA do local}
+#'   \item{province}{Província onde se situa o local}
+#'   \item{district}{Distrito em que o local está situado}
+#'   \item{datim_name}{Nome do local no Datim}
+#'   \item{mozart_name}{Nome do local no MozART}
 #' }
 "data_location_lookup"
 
 
-#' data_type_id_lookup
+#' Referência: data_type_id_lookup
 #'
-#' List of indicators
+#' Source: \href{https://github.com/usaid-mozambique/sonata/blob/main/R/data.R}{`R/data.R`}.
 #'
 #' @usage data(data_type_id_lookup)
 #'
 #' @format Quadro de dados com variáveis:
 #' \describe{
-#'   \item{data_type_id_lookup}{id}
-#'   \item{data_type_id_lookup}{table_name}
-#'   \item{data_type_id_lookup}{column_name}
-#'   \item{data_type_id_lookup}{id_type_lookup}
-#'   \item{data_type_id_lookup}{id_type_desc}
-#'   \item{data_type_id_lookup}{id_type_lookup_pt}
-#'   \item{data_type_id_lookup}{notes}
+#'   \item{id}{id}
+#'   \item{table_name}{Nome do formulário específico (por exemplo, form)}
+#'   \item{column_name}{Código único do formulário específico (por exemplo, form_id)}
+#'   \item{id_type_lookup}{Código numérico do formulário específico (por exemplo, 60)}
+#'   \item{id_type_desc}{Descrição do formulário específico - inglês}
+#'   \item{id_type_lookup_pt}{Descrição do formulário específico - português}
+#'   \item{notes}{Notas}
 #' }
 "data_type_id_lookup"
 
 
-#' data_observation_lookup
+#' Referência: data_observation_lookup
 #'
-#' List of indicators
+#' Source: \href{https://github.com/usaid-mozambique/sonata/blob/main/R/data.R}{`R/data.R`}.
 #'
 #' @usage data(data_observation_lookup)
 #'
 #' @format Quadro de dados com variáveis:
 #' \describe{
-#'   \item{data_observation_lookup}{concept_id}
-#'   \item{data_observation_lookup}{concept_name}
+#'   \item{concept_id}{Identificador do conceito}
+#'   \item{concept_name}{Nome do conceito}
 #' }
 "data_observation_lookup"
 
